78498a4acdaca2dad876817a1345e9f7cf8ab3e1
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-09-23 23:04:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,15 +30,15 @@ switch ($GET){ // Access information to determine output
         $TEXT_DATA_LASTEST=file_get_contents("compress.zlib://".$DATAFILE.$DATANAME); // Obtain old temperature and humidity records
         echo '<center>
             <table border="1">
-                <tr><td>时间</td><td>温度(˚C)</td><td>湿度(%)</td><td>天气</td></tr>
+                <tr><td>Time</td><td>Temperature(˚C)</td><td>Humidity(%)</td><td>Weather</td></tr>
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-67,-45)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-36,-34)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-21,-19)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-8,-2)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-136,-114)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-105,-103)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-90,-88)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-77,-71)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-208,-182)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-174,-172)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-159,-157)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-146,-140)).'</td></tr>'.'
-                <tr><td>时间</td><td>温度(˚C)</td><td>湿度(%)</td><td>天气</td></tr>
+                <tr><td>Time</td><td>Temperature(˚C)</td><td>Humidity(%)</td><td>Weather</td></tr>
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-274,-252)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-243,-241)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-228,-226)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-215,-209)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-343,-321)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-312,-310)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-297,-295)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-284,-278)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-412,-390)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-381,-379)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-366,-364)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-353,-347)).'</td></tr>'.'
-                <tr><td>时间</td><td>温度(˚C)</td><td>湿度(%)</td><td>天气</td></tr>
+                <tr><td>Time</td><td>Temperature(˚C)</td><td>Humidity(%)</td><td>Weather</td></tr>
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-481,-459)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-450,-448)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-435,-433)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-422,-416)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-550,-528)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-519,-517)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-504,-502)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-491,-485)).'</td></tr>'.'
                 <tr><td>'.urldecode(substr($TEXT_DATA_LASTEST,-619,-597)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-588,-586)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-573,-571)).'</td><td>'.urldecode(substr($TEXT_DATA_LASTEST,-560,-554)).'</td></tr>'.'
@@ -46,11 +46,11 @@ switch ($GET){ // Access information to determine output
             </center><br>';
         /*    end:Old temperature and humidity records    */
         echo "<center><a href='".$DATAFILE.$DATANAME."'download='data.txt'><button style='width:200px;height:60px;'><font size='5'>日志下载</font></button></a></center>";
-        //echo '<center><h4>当前使用API:&nbsp;'.$API.'</h4></center>'; // Show APIs currently in use
+        echo '<center><h4>Current API:&nbsp;'.$API.'</h4></center>'; // Show APIs currently in use
     break;
     case 'CRON':
         echo '<meta http-equiv="refresh" content="3;url=/">'; // Jump Page
-        echo '<title>Cron</title><style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #4a86e8; color: #fff;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>计划任务执行成功!&nbsp;<br/></p><span style="font-size: 25px">页面将在3秒后跳转</span></div>'; // Display capture completion prompt
+        echo '<title>Cron</title><style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #4a86e8; color: #fff;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>Cron Success!&nbsp;<br/></p><span style="font-size: 25px">The page will jump in 3 seconds.</span></div>'; // Display capture completion prompt
         file_put_contents($DATAFILE.$DATANAME,"\r\n".$DATETIME.' 温度: '.$DATA['now']['temp'].'˚C; 湿度: '.$DATA['now']['humidity'].'%'.'; 天气: 失败; ',FILE_APPEND); // Update file
     break;
 }
